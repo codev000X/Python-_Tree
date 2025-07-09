18c92be484f6b0e22eba3688ab0455b8829c85f7
Shahzaib <heneryjeans1101@gmail.com>
2025-07-09 12:47:25 +0500

    comments added for ease

--- tree.py
+++ tree.py
@@ -3,13 +3,14 @@ class TreeNode:
         self.data = data
         self.children = children
 
+    # This function adds the child node to the parental node.
+    
     def addChild(self , TreeNode):
         self.children.append(TreeNode)
 
 
     def __str__(self , level=0):
-        res = ' ' + level + str(self.data) + '\n'
+        res = '  ' * level + str(self.data) + '\n'
         for child in self.children:
-            res += child.__str__(level + 1)
-        return res
-
+            res += child.__str__(level + 1)      # This loop calls the  __str__  fuction recursively 
+        return res                               # to add the spaces and new line for the respective childs .
