d3a25230139edf4a78ff2d02fca84566c795e65f
Shahzaib <heneryjeans1101@gmail.com>
2025-07-13 10:25:11 +0500

    feat : Implement the logic for the insertion of node .

--- BinaryTreePL.py
+++ BinaryTreePL.py
@@ -1,2 +1,14 @@
 class BinaryTree:
     def __init_(self , size):
+        self.customList = size * [None]
+        self.LastUsedIndex = 0
+        self.maxSize = size
+
+    def insertNode(self , value):
+        
+        if self.LastUsedIndex+1 = self.maxSize:
+            return "Tree is full"
+        else:
+            self.customList[self.LastUsedIndex + 1] = value
+            self.LastUsedIndex += 1
+            return "Value inserted successfully"
