e85a4bd9b49a8bef3c8b41226b9d0ce4c68e6fa4
Shahzaib <heneryjeans1101@gmail.com>
2025-07-13 12:54:16 +0500

    feats : Methods to search in the Binary Tree made from python list .

--- BinaryTreePL.py
+++ BinaryTreePL.py
@@ -12,3 +12,31 @@ class BinaryTree:
             self.customList[self.LastUsedIndex + 1] = value
             self.LastUsedIndex += 1
             return "Value inserted successfully"
+
+    def searchNode(self, node):
+        return 'Success' if node in self.customList else 'Not found.'
+
+    def preOrderTraversal(self , index):
+        if index > self.LastUsedIndex:
+            return
+        print(self.customList[index])
+        self.preOrderTraversal(index*2)
+        self.preOrderTrveral(index*2 + 1)
+
+    def inOrderTraveral(self , index):
+        if index > self.LastUsedIndex:
+            return
+        self.preOrderTraversal(index*2)
+        print(self.customList[index])
+        self.preOrderTrveral(index*2 + 1)
+
+    def postOrderTraversal(self , index):
+        if index > self.LastUsedIndex:
+            return
+        self.preOrderTraversal(index*2)
+        self.preOrderTrveral(index*2 + 1)
+        print(self.customList[index])
+
+    def levelOrderTraversal(self , index):
+        for i in range(index , self.lastUsedIndex + 1):
+            print(self.customlist[i])
