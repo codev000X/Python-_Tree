d762b2344d48aa93aa08d93e3e60a136a2a7cb15
Shahzaib <heneryjeans1101@gmail.com>
2025-07-19 09:43:10 +0500

    Method for insertion in the Binary Heap added

--- Binary_Heap.py
+++ Binary_Heap.py
@@ -24,3 +24,26 @@ def levelOrderTraversal(rootNode):
         for i in range(1 , rootNode.heapSize+1):
             print(i)
 
+def heapifyTreeInsert(rootNode , index , heapType):
+    if not rootNode or index <= 1 or index > rootNode.heapSize:
+        return
+
+    parentIndex = index // 2
+
+    if heapType == "min":
+        if rootNode.customList[index] < rootNode.customList[parentIndex]:
+            rootNode.customList[index], rootNode.customList[parentIndex] = rootNode.customList[parentIndex], rootNode.customList[index]
+            heapifyTreeInsert(rootNode, parentIndex, heapType)
+    elif heapType == "max":
+        if rootNode.customList[index] > rootNode.customList[parentIndex]:
+            rootNode.customList[index], rootNode.customList[parentIndex] = rootNode.customList[parentIndex], rootNode.customList[index]
+            heapifyTreeInsert(rootNode, parentIndex, heapType)
+
+def insertNode(nodeValue , rootNode , heapType):
+    if not rootNode or rootNode.heapSize + 1 >= rootNode.maxSize:
+        return
+
+    rootNode.customList[rootNode.heapSize + 1] = nodeValue
+    rootNode.heapSize += 1
+    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)
+
